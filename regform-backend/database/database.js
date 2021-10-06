1827510e7864a4202e2e112e3043448ed3161c94
caglardur <cagi666@gmail.com>
2021-10-06 14:33:15 +0300

    Update database.js

--- regform-backend/database/database.js
+++ regform-backend/database/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2")
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "reglist",
-  password: "555556"
+  host: "",
+  user: "",
+  database: "",
+  password: ""
 })
 
 module.exports = pool.promise()
